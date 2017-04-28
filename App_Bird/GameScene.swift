b07400229ca281eb3b0a8632768db1efed2f0230
cice <cice@aula8PovMacN09.local>
2017-04-28 17:23:27 +0200

    today

--- App_Bird/GameScene.swift
+++ App_Bird/GameScene.swift
@@ -24,11 +24,13 @@ class GameScene: SKScene {
         makeLimitLand()
         makeBird()
         makeBackground()
+        makeLoopPipe1AndPipe2()
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 70))
     }
     
     
@@ -40,7 +42,7 @@ class GameScene: SKScene {
     //MARK: - utils
     
     func makeLimitLand(){
-        limitLand.position = CGPoint(x: -(self.frame.width), y: self.frame.height)
+        limitLand.position = CGPoint(x: -(self.frame.width/2), y: -(self.frame.height/2))
         limitLand.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width, height: 1))
         limitLand.physicsBody?.isDynamic = false
         limitLand.zPosition = 2
@@ -83,11 +85,52 @@ class GameScene: SKScene {
         
         //5 -> posición en el espacio
         bird.position = CGPoint(x: 0, y: 0)
-        bird.zPosition = 1
+        bird.zPosition = 15
+        
+        //GRUPO DE FISICAS
+        //bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.width / 2)
+        bird.physicsBody = SKPhysicsBody(texture: birdTexture1, alphaThreshold: 0.5, size: CGSize(width: bird.size.width, height: bird.size.height))
+        
+        bird.physicsBody?.isDynamic = true
+        bird.physicsBody?.allowsRotation = false
+        
+        
         
         //6 -> animación
         bird.run(makeBirdForever)
         self.addChild(bird)
+        
+        
+    }
+    
+    
+    func makePipeFinal(){
+        
+        let gapHeight = bird.size.height * 4
+        let movementAmount = arc4random_uniform(UInt32(self.frame.height / 2))
+        let pipeOffset = CGFloat(movementAmount) - self.frame.height / 4
+        
+        //mover la tubería
+        let movePipes = SKAction.moveBy(x: -self.frame.width, y: 0, duration: TimeInterval(self.frame.width / 200))
+        let removePipes = SKAction.removeFromParent()
+        let moveAndRemovePipes = SKAction.sequence([movePipes, removePipes])
+        
+        //creamos el pipe arriba
+        let pipetexture1 = SKTexture(imageNamed: "pipe1")
+        pipeFinal1 = SKSpriteNode(texture: pipetexture1)
+        pipeFinal1.position = CGPoint(x: self.frame.width / 2, y: (pipeFinal1.size.height / 2) + (gapHeight / 2) + pipeOffset)
+        pipeFinal1.physicsBody = SKPhysicsBody(rectangleOf: pipeFinal1.size)
+        pipeFinal1.physicsBody?.isDynamic = false
+        pipeFinal1.zPosition = 4
+        pipeFinal1.run(moveAndRemovePipes)
+        self.addChild(pipeFinal1)
+        
+    }
+    
+    func makeLoopPipe1AndPipe2(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(makePipeFinal), userInfo: nil, repeats: true)
+        
         
         
     }
